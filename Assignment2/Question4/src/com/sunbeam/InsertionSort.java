ef13296c48b6e47ff121282aa28bd52249283778
kd3-80115 <amkadnan89@gmail.com>
2023-10-27 15:41:52 +0530

    Assignment 2 Question 4 Uploaded

---
+++ Assignment2/Question4/src/com/sunbeam/InsertionSort.java
@@ -0,0 +1,37 @@
+package com.sunbeam;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static int insertionSort(int arr[],int n)
+	{
+		int count=0;
+		for(int i=1;i<n;i++)
+		{
+			int temp=arr[i];
+			int j=i-1;
+			while(j>=0 && arr[j]<temp)
+			{
+				count++;
+				arr[j+1]=arr[j];
+				j--;
+				System.out.println(Arrays.toString(arr));
+			}
+			arr[j+1]=temp;
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		
+		int[] arr = { 55, 44, 22, 11 ,66, 33 };
+		System.out.println("Before Bubble sort");
+		System.out.println(Arrays.toString(arr));
+		System.out.println("After Bubble sort");
+		System.out.println("No. of comparison:" + insertionSort(arr, arr.length));
+		System.out.println(Arrays.toString(arr));
+		System.out.println("No. of comparison on sorted array:" + insertionSort(arr, arr.length));
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
